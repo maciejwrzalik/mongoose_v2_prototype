7b401d82f7f68b2b83ff7628ac2346b6a7e2bbfe
Maciej Wrzalik <maciej.wrzalik@infor.com>
2026-02-05 12:34:40 +0100

    Improve radio group styling and column alignment

--- components/radio-group-component.js
+++ components/radio-group-component.js
@@ -3,17 +3,20 @@ class RadioGroupComponent extends NodeComponent {
   render() {
     const labels = ['Poland','United States','Nederlands','Republic of South Africa','United Kingdom','Sweden','Germany','Norway'];
     const wrapper = document.createElement('div');
-    wrapper.className = 'radio-group';
+    wrapper.className = 'radio-group-wrapper';
+    
+    // Label (styled to match input label)
     const label = document.createElement('label');
     label.className = 'comp-label';
     label.textContent = this.name;
     wrapper.appendChild(label);
 
+    // Options container (vertical layout)
     const options = document.createElement('div');
-    options.className = 'radio-options';
+    options.className = 'radio-group-options';
     for (let i = 1; i <= 4; i++) {
       const opt = document.createElement('label');
-      opt.className = 'radio-option';
+      opt.className = 'radio-group-option';
       const rb = document.createElement('input');
       rb.type = 'radio';
       rb.name = this.id;
